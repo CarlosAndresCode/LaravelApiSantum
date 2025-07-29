692650b673a1ab3e4d14784c45b25465c91d3b85
Carlos Andres Pacheco Fuentes <carlosandres4020@gmail.com>
2025-07-28 23:05:27 -0500

    group post and comment routes under authentication middleware and add a logout route

--- routes/api.php
+++ routes/api.php
@@ -10,13 +10,19 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('posts', PostController::class);
-Route::apiResource('comments', CommentController::class);
-
 Route::post('/users/register', [AuthController::class, 'register']);
 Route::post('/users/login', [AuthController::class, 'login']);
 
 Route::middleware('signed')
     ->get('/email/verify/{id}/{hash}', [AuthController::class, 'verify'])
     ->name('verification.verify');
+
+Route::middleware('auth:sanctum')
+    ->post('/users/logout', [AuthController::class, 'logout'])
+    ->name('logout');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('posts', PostController::class);
+    Route::apiResource('comments', CommentController::class);
+});
 
